52843f67abdf20336f81af1438945d4554573338
Yunfan Kang <86796489+YunfanKang@users.noreply.github.com>
2024-03-31 13:19:37 -0500

    add likehood calculation for poission

--- lib/network_scan.py
+++ lib/network_scan.py
@@ -8,5 +8,19 @@ def likelyhood_ratio(baseline, observed):
     if observed > baseline:
         return np.power((observed / baseline), observed) * np.exp(baseline - observed)
     return 1.0
-
-def network_scan(G, center, subnetwork_size):
+def continuous_homogeneous_possion_likelyhood_test(baseline, observed, total_baseline, total_observed):
+    if observed < 0:
+        raise ValueError("The number of observed events is negative")
+    if baseline < 0:
+        raise ValueError("The number of simulated events is negative")
+    if observed > baseline:
+        return (np.power((observed / baseline), observed) * np.power((total_observed - observed)/ (total_baseline - baseline), total_observed - observed)) / np.power((total_observed/total_baseline), total_observed)
+    
+    return 1.0
+    
+    
+def expected_counts(study_G_size, G_size, G_counts):
+    return G_counts * study_G_size / G_size
+def network_scan(G, sub_G):
+    #It should be independent of the definition of the search window / subnetwork
+    total_Count = 
